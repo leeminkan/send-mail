250a33e7092a02811b0df511957ed693dfb783a7
leeminkan <16520950@gm.uit.edu.vn>
2020-09-24 11:22:22 +0700

    update code

--- src/mail.php
+++ src/mail.php
@@ -3,21 +3,7 @@
 namespace leeminkan\SendMail;
 
 class Mail {
-    // Properties
-    public $mail_service;
-
-    function __construct($mail_service) {
-        $this->mail_service = $mail_service;
-    }
     
-    // Methods
-    function set_mail_service($mail_service) {
-        $this->mail_service = $mail_service;
-    }
-
-    function get_mail_service() {
-        return $this->mail_service;
-    }
 }
 
 ?>
